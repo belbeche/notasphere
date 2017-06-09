7317e5bdd0df96d096c8e64be70ea5b02192a4e0
wBelbeche <belbeche.w@gmail.com>
2017-06-09 18:38:06 +0200

    Modifications CONTENU

--- contenu.php
+++ contenu.php
@@ -1,19 +1,33 @@
+<div class="alert alert-info" role="alert">
+       <center> <strong>Développement en local &copy W.B</strong>. </center>
+      </div>
+<br/>
+<div class="container">
+	<div class="page-header">
+		<h1 class="etude">Une étude généraliste avec des savoir-faire de spécialistes</h1>
+	</div>
+	<div class="well">
+		<p>L'Office est détenteur des minutes de Me Philippe HEGNER, Me BELOTTE et de Me MISSET.</p>
+		<p>L'Office Notarial de DIJON se situe Passage DARCY.</p>
+	</div>
+</div>
 <div class="container">
 	<hr>
 	<div class="row">
-		<div class="col-sm-4">
+		<div class="col-xm-4">
 	      <div class="list-group">
 	        <a href="#" class="list-group-item active">
-	          <h4 class="list-group-item-heading">L'OFFICE</h4>
-	          <p class="list-group-item-text">Informations:</p>
+	          <h4 class="list-group-item-heading">PARTICULIERS</h4>
+	          <p class="list-group-item-text">Témoins et confidents privilégiés de toute votre vie familiale et professionnelle, nous avons à cœur de vous accompagner dans vos projets : transaction immobilière, gestion patrimoniale, fiscalité, succession, transmission.</p>
 	        </a>
 	        <a href="#" class="list-group-item">
-	          <h4 class="list-group-item-heading">Services</h4>
-	          <p class="list-group-item-text">Description:</p>
+	          <h4 class="list-group-item-heading">ROFESSIONNELS</h4>
+	          <p class="list-group-item-text">Notre organisation en pôles de compétences nous permet de répondre avec réactivité à vos besoins.
+Dans les opérations complexes, une équipe dédiée sera mise en place pour traiter vos projets de façon transversale.</p>
 	        </a>
 	        <a href="#" class="list-group-item">
-	          <h4 class="list-group-item-heading">L'équipe</h4>
-	          <p class="list-group-item-text">Description.</p>
+	          <h4 class="list-group-item-heading">INSTITUTIONNELS</h4>
+	          <p class="list-group-item-text">Responsables de Collectivités territoriales, d'établissements publics, de groupes bancaires, d'associations et de fondations. Nous vous offrons une palette complète de missions d’assistance, de conseils, de services et de rédaction de contrats adaptés à vos besoins.</p>
 	        </a>
 	      </div>
 	    </div><!-- /.col-sm-4 -->
